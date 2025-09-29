5b94c8ea4124d8feb77e011141e0f186e65b6266
Jesusda11 <danieloarias11@gmail.com>
2025-09-28 21:15:14 -0500

    fix(cors): enable all origins

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ const database = require('./config/database');
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./router/routes');
+const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
 const app = express();
 
-// Middleware : POST,PUT,PATCH
+app.use(cors({
+  origin: '*',   
+  credentials: false, 
+}));
+
 app.use(bodyParser.json());
-app.use('/api/v1', routes)
 
+app.use('/api/v1', routes);
 
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-    database();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  database();
 });
