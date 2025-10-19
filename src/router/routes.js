7c7e0727eb9fbc275aa8e4a6de5b6ab450ae0d98
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2025-10-18 23:55:11 -0500

    fix: correct router initialization in routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,10 @@ const authRoutes = require("./authRoutes");
 const adminRoutes = require("./adminRoutes");
 const userRoutes = require("./userRoutes");
 
+const router = express.Router();
+
 router.use("/auth", authRoutes);
 router.use("/admin", adminRoutes);
 router.use("/users", userRoutes);
 
 module.exports = router;
-
