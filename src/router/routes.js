0cac307034c397d27b735588588e843a6a7f260b
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2025-10-18 22:51:18 -0500

    feat: get user by id implemented.

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,10 @@
-const express = require("express")
-const router = express.Router()
-const authRoutes = require("./authRoutes")
-const userRoutes = require("./userRoutes")
-const adminRoutes = require("./adminRoutes")
-const pacientesRoutes = require("./pacientesRoutes");
-const doctorRoutes = require("./doctorRoutes");
-const nurseRoutes = require("./nurseRoutes");
+const express = require("express");
+const router = express.Router();
+const authRoutes = require("./authRoutes");
+const userRoutes = require("./userRoutes");
 
-router.use("/pacientes", pacientesRoutes);
+router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
 
-router.use("/auth", authRoutes)
-
-router.use("/admin", adminRoutes)   
-
-router.use("/doctors", doctorRoutes);
-
-router.use("/nurses", nurseRoutes);
 module.exports = router;
+
--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const router = express.Router();
 
+const doctorRoutes = require("./doctorRoutes");
+const nurseRoutes = require("./nurseRoutes");
+const { getUserById } = require("../controllers/UserController");
+
+router.get("/:userId", getUserById);
+
+router.use("/doctors", doctorRoutes);
+router.use("/nurses", nurseRoutes);
+
+module.exports = router;
